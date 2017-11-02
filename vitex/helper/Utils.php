09dcab4893403792488195a407e7fa65f7f3280a
skipify <skipify@users.noreply.github.com>
2017-11-02 17:50:18 +0800

    更换了加密为openssl
    更换了加密为openssl

--- vitex/helper/Utils.php
+++ vitex/helper/Utils.php
@@ -19,35 +19,28 @@ use vitex\core\Exception;
  */
 class Utils
 {
-    const CRYPT_DEFAULT = ['algorithm' => MCRYPT_RIJNDAEL_256, 'mode' => MCRYPT_MODE_CBC];
+    const CRYPT_DEFAULT = ['cipher' => 'aes-256-cbc', 'options' => OPENSSL_RAW_DATA,'iv'=>'0000000000000000'];
+
     /**
      * 加密数据
      * @param  string $data 要加密的数据
      * @param  string $key 加密的密钥
      * @param  array $setting 配置文件，配置加密的模式和加密的方法
-     * @return string  加密的数据
+     * @return string 加密的数据
+     * @throws Exception
      */
     public static function encrypt($data, $key, array $setting = [])
     {
-        $data    = serialize($data);
+        if(!is_string($data)){
+            throw  new Exception(Exception::CODE_PARAM_ERROR_MSG,Exception::CODE_PARAM_VALUE_ERROR);
+        }
+
         $setting = array_merge(Utils::CRYPT_DEFAULT, $setting);
-        /* 打开加密模块，并且创建初始向量 */
-        $mmo = mcrypt_module_open($setting['algorithm'], '', $setting['mode'], '');
-        $key = substr($key, 0, mcrypt_enc_get_key_size($mmo));
-        //创建加密向量
-        $iv_size = mcrypt_enc_get_iv_size($mmo);
-        $iv      = mcrypt_create_iv($iv_size);
-        /* 初始化加密句柄 */
-        mcrypt_generic_init($mmo, $key, $iv);
-        /* 加密数据 */
-        $value = mcrypt_generic($mmo, $data);
-        mcrypt_generic_deinit($mmo);
-        mcrypt_module_close($mmo);
-        $iv    = base64_encode($iv);
-        $value = base64_encode($value);
-        $hmac  = hash_hmac('sha256', $iv.$value, $key);
-        $json  = json_encode(compact('iv', 'value','hmac'));
-        return base64_encode($json);
+        $encrypted = openssl_encrypt($data,$setting['cipher'],$key,$setting['options'],$setting['iv']);
+        if($encrypted === false){
+            throw new Exception('Can not encrypt data',Exception::CODE_PARAM_VALUE_ERROR);
+        }
+        return base64_encode($encrypted);
     }
 
     /**
@@ -60,32 +53,31 @@ class Utils
      */
     public static function decrypt($endata, $key, array $setting = [])
     {
-        $data = json_decode(base64_decode($endata), true);
-        if (!is_array($data) || !isset($data['iv'], $data['value'], $data['hmac'])) {
-            throw new Exception("无法解密数据",Exception::CODE_PARAM_PARSE_ERROR);
+        if(!is_string($endata)){
+            throw  new Exception(Exception::CODE_PARAM_ERROR_MSG,Exception::CODE_PARAM_VALUE_ERROR);
         }
-        $hmac  = $data['hmac'];
-        $iv    = base64_decode($data['iv']);
-        $value = base64_decode($data['value']);
-
-        //验证 hmac
-        $checkKey  = substr(md5(time()),12,16);
-        $calcHmac  = hash_hmac('sha256', hash_hmac('sha256', $iv.$value, $key), $checkKey, true);
-
-        if(hash_equals(hash_hmac('sha256', $hmac, $checkKey, true), $calcHmac)) {
-            throw new Exception("无法解密数据",Exception::CODE_PARAM_PARSE_ERROR);
-        }
-
         $setting = array_merge(Utils::CRYPT_DEFAULT, $setting);
-        /* 打开加密模块，并且创建初始向量 */
-        $mmo = mcrypt_module_open($setting['algorithm'], '', $setting['mode'], '');
-        $key = substr($key, 0, mcrypt_enc_get_key_size($mmo));
+        $data = openssl_decrypt(base64_decode($endata), $setting['cipher'], $key, $setting['options'],$setting['iv']);
+        if($data === false){
+            throw new Exception('Can not decrypt data',Exception::CODE_PARAM_VALUE_ERROR);
+        }
+        return $data;
+    }
 
-        /* 初始化加密句柄 */
-        mcrypt_generic_init($mmo, $key, $iv);
-        $dedata = mdecrypt_generic($mmo, $value);
-        mcrypt_generic_deinit($mmo);
-        mcrypt_module_close($mmo);
-        return unserialize($dedata);
+    /**
+     * 判断PHP版本是否>=某一个版本
+     * @param $version
+     * @return bool
+     */
+    public static function phpVersion($version)
+    {
+        static $phpVerisons = [];
+
+        $version = (string)$version;
+
+        if (!isset($phpVerisons[$version])) {
+            $phpVerisons[$version] = version_compare(PHP_VERSION, $version, '>=');
+        }
+        return $phpVerisons[$version];
     }
 }
