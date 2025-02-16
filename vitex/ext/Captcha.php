6941a91f95ebcac8209de9be73e7015d078a7239
duzhenlin <duzhenlin@vip.qq.com>
2025-02-16 14:18:42 +0800

    fix-php验证码，在严格模式下，类型不符问题

--- vitex/ext/Captcha.php
+++ vitex/ext/Captcha.php
@@ -1,4 +1,5 @@
 <?php declare(strict_types=1);
+
 namespace vitex\ext;
 
 class Captcha
@@ -18,7 +19,7 @@ class Captcha
      * 图片的宽高像素
      * @var integer
      */
-    private $width  = 80;
+    private $width = 80;
     private $height = 40;
     /**
      * 干扰线数量
@@ -31,6 +32,7 @@ class Captcha
      * @var string
      */
     private $engine = '';
+
     public function __construct($setting = [])
     {
         $this->font = dirname(__FILE__) . '/font.ttf';
@@ -41,16 +43,18 @@ class Captcha
             $this->{$key} = $val;
         }
     }
+
     /**
      * 生成验证码
-     * @param  string $file 图片保存文件名，不指定则图片会被直接输出
+     * @param string $file 图片保存文件名，不指定则图片会被直接输出
      * @return null
+     * @throws \ImagickException
      */
     public function get($file = "")
     {
-        $support             = $this->support();
-        list($code, $answer) = $this->generateCode();
-        $image               = null;
+        $support = $this->support();
+        [$code, $answer] = $this->generateCode();
+        $image = null;
         @session_start();
         $_SESSION['vitex.captcha.answer'] = strtolower($answer);
         if ($support == 'imagick') {
@@ -81,9 +85,10 @@ class Captcha
             }
         }
     }
+
     /**
      * 验证码检查
-     * @param  string $code               输入的验证码结果
+     * @param string $code 输入的验证码结果
      * @return bool   true为验证通过 false为验证失败
      */
     public static function test($code)
@@ -98,34 +103,36 @@ class Captcha
         }
         return false;
     }
+
     /**
      * 生成验证码内容以及验证答案
      * @return array code 返回生成的验证码
      */
-    protected function generateCode()
+    protected function generateCode(): array
     {
         $answer = '';
         if ($this->type == "1") {
-            $code   = md5(time());
+            $code   = md5((string)time());
             $code   = substr($code, rand(3, 5), $this->length);
             $code   = str_replace(['2', '0', 'o', '1', 'i'], ['Z', '9', '8', 'L', 'A'], $code);
             $code   = strtoupper($code);
             $answer = $code;
         } else {
-            $operate               = ['+', '-'];
-            $opindex               = rand(0, 1);
-            $opnum1                = rand(0, 9);
-            $opnum2                = rand(0, 9);
-            list($opnum1, $opnum2) = ($opindex == 1 ? [max($opnum1, $opnum2), min($opnum1, $opnum2)] : [$opnum1, $opnum2]);
-            $code                  = $opnum1 . $operate[$opindex] . $opnum2 . '=?';
-            $answer                = $opindex == 1 ? ($opnum1 - $opnum2) : ($opnum1 + $opnum2);
+            $operate = ['+', '-'];
+            $opindex = rand(0, 1);
+            $opnum1  = rand(0, 9);
+            $opnum2  = rand(0, 9);
+            [$opnum1, $opnum2] = ($opindex == 1 ? [max($opnum1, $opnum2), min($opnum1, $opnum2)] : [$opnum1, $opnum2]);
+            $code   = $opnum1 . $operate[$opindex] . $opnum2 . '=?';
+            $answer = $opindex == 1 ? ($opnum1 - $opnum2) : ($opnum1 + $opnum2);
         }
-        return [$code, $answer];
+        return [$code, (string)$answer];
     }
+
     /**
      * 在图片上添加验证文字
-     * @param  resource $image         图片对象
-     * @param  string   $text          要添加的字符
+     * @param resource $image 图片对象
+     * @param string   $text  要添加的字符
      * @return resource 图片对象
      */
     protected function imagickDrawText($image, $text)
@@ -143,21 +150,22 @@ class Captcha
         $draw->destroy();
         return $image;
     }
+
     protected function gdDrawText($image, $text)
     {
         //干扰字符
-        $code  = md5(time());
+        $code  = md5((string)time());
         $color = imagecolorallocate($image, rand(180, 200), rand(180, 200), rand(180, 200));
-        imagettftext($image, $this->height * 0.8, 3, $this->width * 0.05 + 1, $this->height * 0.8 + 2, $color, $this->font, $code);
+        imagettftext($image, $this->height * 0.8, 3, (int)round($this->width * 0.05 + 1), (int)round($this->height * 0.8 + 2), $color, $this->font, $code);
         //实际验证码
         $color = imagecolorallocate($image, rand(30, 120), rand(30, 120), rand(30, 120));
-        imagettftext($image, $this->height * 0.6, 3, $this->width * 0.05, $this->height * 0.8, $color, $this->font, $text);
+        imagettftext($image, $this->height * 0.6, 3, (int)round($this->width * 0.05), (int)round($this->height * 0.8), $color, $this->font, $text);
     }
 
     /**
      * 在图片上画线
-     * @param $image
-     * @param  integer $num 画线数量
+     * @param         $image
+     * @param integer $num 画线数量
      * @return object 图片对象
      * @internal param object $img 图片对象
      */
@@ -174,10 +182,11 @@ class Captcha
             imageline($image, $x1, $y1, $x2, $y2, $color);
         }
     }
+
     /**
      * 在图片上划线
-     * @param  object  $image             Imagick的实例
-     * @param  integer $num               画线的数量
+     * @param object  $image Imagick的实例
+     * @param integer $num   画线的数量
      * @return object  Imagick的实例
      */
     protected function imagickLine($image, $num = 4)
